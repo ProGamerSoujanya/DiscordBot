9ce97c16e75430abf7b6ca4accd371467287189a
SoujanyaChaudhuri <86586562+SoujanyaChaudhuri@users.noreply.github.com>
2023-03-26 21:54:51 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const {Client, Events, GatewayIntentBits} = require('discord.js');
+
+
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.on('ready', () =>
+         console.log(`Logged in as ${client.user.tag}`)
+         )
+
+client.login(process.env.TOKEN)
